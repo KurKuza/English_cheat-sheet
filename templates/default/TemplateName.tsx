97c5d78a3f4a2b2187e22f83ea6a9f4b1038050c
Kurkulio <kurkuliogo@gmail.com>
2022-09-11 22:41:21 +0300

    generate component

--- templates/default/TemplateName.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './TemplateName.module.css'
-
-const TemplateName = () => (
-	<div className={styles.templateName}>
-		<h1>TemplateName component</h1>
-	</div>
-)
-
-export default TemplateName
